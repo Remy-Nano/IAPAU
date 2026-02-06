df87e7261538801f062316c1165e9f81af5be38c
matheoalves <matheoalves01@gmail.com>
2026-02-06 22:21:42 +0100

    chore: optimize docker image

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: "standalone",
 
   // ⛔ Désactiver ESLint pendant les builds Docker
   eslint: {
